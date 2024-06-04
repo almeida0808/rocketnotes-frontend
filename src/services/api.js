e2bafa701e6d362dfb723e2a8a8f5172d7e4974f
almeida0808 <almeidaff08@mail.com>
2024-06-04 10:04:24 -0300

    novo endereço do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"; // permite a coneão com o backend
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333", // colocar o endereçõ do backend , lembrando que ele tem que estar rodadndo também
+  baseURL: "https://rocketnotes-almeida-dev.onrender.com", // colocar o endereçõ do backend , lembrando que ele tem que estar rodadndo também
   // tendo um baseURL não precisamos mais informar qual o dominio da nossa API
 });
